e581a38007be3326aeabe246b49ae9b4adb45f15
Victor Bofill <victorm.bofill@gmail.com>
2019-07-05 15:12:57 -0700

    process hello world

--- src/components/process/Process.js
+++ src/components/process/Process.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import './Process.scss';
 
 export default class Process extends Component {
   render() {
     return (
-      <article className="process">
-          <h1>Process</h1>
-      </article>
-    );
+      <Fragment>
+        <figure className="process-figure">
+          <img />
+          <figcaption>Some text fluff bullshit about how we think and design blah blah blah.</figcaption>
+        </figure>
+      </Fragment>
+  );
   }
 }
