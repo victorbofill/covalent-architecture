257a866a57e021c114cf2b75487ee7ce6487426c
Victor Bofill <victorm.bofill@gmail.com>
2019-07-05 21:00:38 -0700

    added css config to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(scss)$/,
+        test: /\.(scss|css)$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
