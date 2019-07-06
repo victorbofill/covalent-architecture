6a0ba78febbc3da10e57369069ad6983ba2eddbc
Victor Bofill <victorm.bofill@gmail.com>
2019-07-06 16:11:45 -0700

    removed local images from Projects.js

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
-
-import { images } from '../../assets/projects/residential/suites/suitesImages';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import { Carousel } from 'react-responsive-carousel';
 
 import './Projects.scss';
 
@@ -11,16 +9,7 @@ export default class Projects extends Component {
   render() {
     return (
       <article className="projects">
-        <Carousel className="carousel">
-          {images.map(image => {
-            return (
-              <div key={image} >
-                <img src={image} />
-                <p className="legend">Legend 1</p>
-              </div>
-            );
-          })}
-        </Carousel>
+        <h1>Projects</h1>
       </article>
     );
   }
