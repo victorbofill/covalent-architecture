2213d255edf585968b495ec4ae0c3c48d395f339
Victor Bofill <victorm.bofill@gmail.com>
2019-07-05 21:03:59 -0700

    hello work Carousel

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+import { images } from '../../assets/projects/commerical/food/foodImages';
+
 import './Projects.scss';
 
 export default class Projects extends Component {
   render() {
     return (
       <article className="projects">
-        <h1>Projects</h1>
+        <Carousel>
+          {images.map(image => {
+            return (
+              <div key={image} >
+                <img src={image} />
+                <p className="legend">Legend 1</p>
+              </div>
+            );
+          })}
+        </Carousel>
       </article>
     );
   }
