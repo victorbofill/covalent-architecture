528fa033dcd4eb30b8e294db90f46e394c60d6ba
Victor Bofill <victorm.bofill@gmail.com>
2019-07-06 20:05:51 -0700

    refactored api on the front end

--- src/services/dropbox/api.js
+++ src/services/dropbox/api.js
@@ -1,19 +1,11 @@
-function request(url, options = {}, data) {
-  if (data) options.body = JSON.stringify(data);
-
-  return fetch(url, options)
-    .then(response => [response.ok, response.json()])
-    .then(([ok, json]) => {
-      if (ok) return json;
-      throw json.message || json.error || json.errors || json;
-    });
-}
-
-const get = (options = {}) => request('/api/dropbox', { method: 'GET', options });
-
 const api = {
-  getTestData: () => {
-    return get();
+  getImageLinks: async (category, project) => {
+    return await fetch(`/api/dropbox/${category}/${project}`, {})
+      .then(response => [response.ok, response.json()])
+      .then(([ok, json]) => {
+        if (ok) return json;
+        throw json.message || json.error || json.errors || json;
+      });
   },
 };
 
