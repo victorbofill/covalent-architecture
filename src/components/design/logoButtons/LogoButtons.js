be30fa14386c8295c9733f25b2b431503ff9cede
Victor Bofill <victorm.bofill@gmail.com>
2019-07-08 18:29:32 -0700

    added specific links to project buttons

--- src/components/design/logoButtons/LogoButtons.js
+++ src/components/design/logoButtons/LogoButtons.js
@@ -8,11 +8,11 @@ export default class Story extends Component {
   render() {
     return (
       <figure className="button-container">
-        <NavLink to="/design/projects"><div className="institutional button"><p>Institutional</p></div></NavLink>
-        <NavLink to="/design/projects"><div className="residential button"><p>Residential</p></div></NavLink>
-        <NavLink to="/design/projects"><div className="commercial button"><p>Commercial</p></div></NavLink>
-        <NavLink to="/design/projects"><div className="industrial button"><p>Industrial</p></div></NavLink>
-        <NavLink to="/design/projects"><div className="tenant button"><p>Tenant Improvements</p></div></NavLink>
+        <NavLink to="/design/projects/institutional"><div className="institutional button"><p>Institutional</p></div></NavLink>
+        <NavLink to="/design/projects/residential"><div className="residential button"><p>Residential</p></div></NavLink>
+        <NavLink to="/design/projects/commercial"><div className="commercial button"><p>Commercial</p></div></NavLink>
+        <NavLink to="/design/projects/industial"><div className="industrial button"><p>Industrial</p></div></NavLink>
+        <NavLink to="/design/projects/tenant"><div className="tenant button"><p>Tenant Improvements</p></div></NavLink>
       </figure>
     );
   }
