307e95b6590e6d0e6806eedba4c0095deeb7cd60
Victor Bofill <victorm.bofill@gmail.com>
2019-07-08 18:56:51 -0700

    fixed a broken link

--- src/components/design/logoButtons/LogoButtons.js
+++ src/components/design/logoButtons/LogoButtons.js
@@ -12,7 +12,7 @@ export default class Story extends Component {
         <NavLink to="/design/projects?residential"><div className="residential button"><p>Residential</p></div></NavLink>
         <NavLink to="/design/projects?commercial"><div className="commercial button"><p>Commercial</p></div></NavLink>
         <NavLink to="/design/projects?industial"><div className="industrial button"><p>Industrial</p></div></NavLink>
-        <NavLink to="/design/projects?tenant"><div className="tenant button"><p>Tenant Improvements</p></div></NavLink>
+        <NavLink to="/design/projects?planning"><div className="tenant button"><p>Tenant Improvements</p></div></NavLink>
       </figure>
     );
   }
