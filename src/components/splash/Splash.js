1f46502ee2381d695aba8542e192b72675e1ab4a
Payton Lower <paytown@me.com>
2019-08-16 12:44:41 -0700

    splash page - unresponsive

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-
 import logo from '../../assets/img/covalent-bond.svg';
-
-import './Splash.scss';
+import styles from './Splash.scss';
 
 export default class Splash extends Component {
   render() {
     return (
-      <article className="splash">
+      <article className={styles.Splash}>
         <figure>
-          <h2><span className="covalent">covalent</span> <span className="architecture">architecture</span></h2>
+          <h2>
+            <span className="covalent">covalent</span> <span className="architecture">architecture</span>
+          </h2>
           <figcaption>
             <span className="term">/ kō vāl ənt / architecture / </span>
             <span className="definition">bonds formed by the sharing of ideas and goals between people</span>
