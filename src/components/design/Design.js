e63e92c54760e52e83fb749671ee2aa890e44449
Payton Lower <paytown@me.com>
2019-08-26 20:26:16 -0700

    design text content

--- src/components/design/Design.js
+++ src/components/design/Design.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import LogoButtons from './logoButtons/LogoButtons';
-import './Design.scss';
+import styles from './Design.scss';
 
 export default class Story extends Component {
   render() {
     return (
-      <article className="design">
+      <article className={styles.design}>
         <LogoButtons />
+        <aside>
+          <p>From commercial to residential<br />
+            planning, design, and architecture,<br />
+            our team will work with you every step of the way.
+          </p>
+          <p>From big projects to small projects,<br />
+            our team’s experience ranges in both size and type.<br />
+            Our experience includes custom residential, multi-family,<br />
+            commercial , and industrial.
+          </p>
+          <p>
+            From future planning to<br />
+            full service design,<br />
+            we can assist from early project phases including land<br />
+            planning to construction documents and administration.
+          </p>
+        </aside>
       </article>
     );
   }
