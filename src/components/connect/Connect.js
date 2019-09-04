79737a2fce7a8469d8979decf3acbb85442c7ad7
Victor Bofill <victorm.bofill@gmail.com>
2019-09-03 21:39:07 -0700

    updated connect

--- src/components/connect/Connect.js
+++ src/components/connect/Connect.js
@@ -21,16 +21,22 @@ export default class Connect extends Component {
           </figure>
           <img />
           <ul className="address-list">
-            <li>General Inquiry: <a href="mailto:hello@covalentarchitecture.com">hello@covalentarchitecture.com</a></li>
             <li>Antoinette Lettiere: <a href="mailto:antoinette@covalentarchitecture.com">antoinette@covalentarchitecture.com</a></li>
-            <li>Melanie Poe: <a href="mailto:melanie@covalentarchitecture.com">melanie@covalentarchitecture.com</a></li>
+            <li>Austin Rosenfeld: <a href="mailto:austin@covalentarchitecture.com">austin@covalentarchitecture.com</a></li>
             <li>James Schmidt: <a href="mailto:james@covalentarchitecture.com">james@covalentarchitecture.com</a></li>
           </ul>
           <div className="address">
-            <h2>mailing address</h2>
-            <p>19215 SE 34th Street</p>
-            <p>#106 - 420</p>
-            <p>Camas, Washington 98607</p>
+            <div>
+              <h2>mailing address</h2>
+              <p>19215 SE 34th Street</p>
+              <p>#106 - 420</p>
+              <p>Camas, Washington 98607</p>
+            </div>
+            <div>
+              <h2>office address</h2>
+              <p>311 W Evergreen Blvd. Suite 110</p>
+              <p>Vancouver, WA 98660</p>
+            </div>
           </div>
         </section>
       </article>
